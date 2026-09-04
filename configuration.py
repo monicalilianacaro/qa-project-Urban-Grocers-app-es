76f85ef3682ce8dfb3c90f4a24c520fe7154b310
Monica Liliana Caro Pereira <monicalilianacaro@hotmail.com>
2026-09-04 18:33:41 -0500

    fix: corregir nombres de archivos y subir gitignore

--- configuration.py
+++ configuration.py
@@ -2,6 +2,6 @@ URL_SERVICE = "https://cnt-9620ee09-0d3f-46d7-9a27-781dcb6f00b4.containerhub.tri
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
 
-#URL_SERVICE: es la direccion base del servidor activo (el servidor donde está alojada la app de pruebas)
+#URL_SERVICE: es la dirección base del servidor activo (el servidor donde está alojada la app de pruebas)
 #CREATE:USER_PATH: Es la ruta (endpoint) para crear usuarios en el sistema
-#KITS_PATH: es la ruta (edfpoint) para crear kits en el sistema
+#KITS_PATH: es la ruta (endpoint) para crear kits en el sistema
